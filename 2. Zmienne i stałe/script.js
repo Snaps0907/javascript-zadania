a6100b32974329cd63e594c648ae9c117742e689
Snaps0907 <57373253+Snaps0907@users.noreply.github.com>
2021-02-06 20:19:28 +0100

    Update script.js
    done

---
+++ 2. Zmienne i stałe/script.js
@@ -0,0 +1,30 @@
+///Zadanie 1
+
+let number=3;
+let string= "Ala ma kota";
+let example=2+"dwa";
+let boolen=true;
+let special=null;
+
+console.log(number);
+console.log(string);
+console.log(example);
+console.log(boolen);
+console.log(special);
+
+///Zadanie 2
+
+let firstNumber=4;
+let secondNumber=8;
+let sum=0;
+
+sum=firstNumber+secondNumber;
+console.log(sum);
+
+///Zadanie 3
+
+let example;
+
+console.log(example);
+
+//wynik: undefined, ponieważ nic co niej nie przypisaliśmy 
