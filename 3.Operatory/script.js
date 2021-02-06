c56cd7f9f5034b31f693e2b61e4e5e7b8975bb53
Snaps0907 <57373253+Snaps0907@users.noreply.github.com>
2021-02-06 20:20:12 +0100

    Update script.js
    done

---
+++ 3.Operatory/script.js
@@ -0,0 +1,45 @@
+///Zadanie 1
+
+let firstBoolen=true;
+let secondBoolen=false;
+console.log(firstBoolen==secondBoolen);
+
+//Zadanie 2
+
+let firstNumber=13;
+let secondNumber=3;
+let moduloResult=0;
+moduloResult=firstNumber%secondNumber;
+console.log(moduloResult);
+
+//Zadanie 3
+
+let firstString="Ala ma";
+let secondString=" kota.";
+let stringsResult="";
+stringsResult=firstString+secondString;
+console.log(stringsResult);
+
+//Zadanie 4
+
+var someNumber = 425;
+var someString ="425";
+console.log(someNumber==someString);
+console.log(someNumber===someString);
+
+//Zadanie 5
+
+let counter=30;
+counter++;
+console.log(counter);
+counter--;
+console.log(counter);
+
+//Zadanie 6
+
+let numberOne=34;
+let numberTwo=4;
+let result=null;
+result=numberOne>numberTwo;
+console.log(result);
+
